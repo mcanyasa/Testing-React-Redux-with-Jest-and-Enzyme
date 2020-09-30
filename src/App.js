2632b8e0923362564cb0b161642ca0289bfb7e34
Mustafa Can Yasa <mustafacan.yasa@dgpays.com>
2020-09-30 11:43:04 +0300

    Initial commit of my components without test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
+import Header from "./components/header/index";
+import './app.scss'
 
 function App() {
   return (
     <div className="App">
-      Test
+      <Header />
     </div>
   );
 }
